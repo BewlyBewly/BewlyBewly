2ce73632fb56d38835cf68d23225bbd268a6e497
Hakadao <a578457889743@gmail.com>
2022-12-11 02:07:57 +0800

    chore: remove unused file

--- src/contentScripts/router/index.ts
+++
@@ -1,9 +0,0 @@
-// import { createRouter, createWebHashHistory } from 'vue-router'
-
-// const routes = [{ path: '/', component: () => import('~/contentScripts/views/Home/index.vue') }]
-
-// export const router = createRouter({
-//   // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-//   history: createWebHashHistory(),
-//   routes, // short for `routes: routes`
-// })
