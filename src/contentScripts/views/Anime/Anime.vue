3f4e97a3686b3b2cbeacdc5eea0115bc96dd7eab
Hakadao <a578457889743@gmail.com>
2023-02-01 02:15:54 +0800

    update: anime page

--- src/contentScripts/views/Anime/Anime.vue
+++ src/contentScripts/views/Anime/Anime.vue
@@ -191,19 +191,68 @@ function getRecommendAnimeList() {
               pr-8
               shrink-0
             >
-              <h3 mb-6 text="$bew-text-2">
-                <span text="2xl" font-bold>{{
-                  daysOfTheWeekList[item.day_of_week - 1]
-                }}</span>
-                <span text="base $bew-text-2" ml-4>{{ item.date }}</span>
-              </h3>
-
-              <ul grid gap-4>
-                <li
-                  v-for="episode in item.episodes"
-                  :key="episode.season_id"
-                >
-                  <div p="x-2 y-1" w="[fit-content]" mb-2 rounded-4 color="$bew-theme-color" bg="$bew-theme-color-20">
+              <div flex mb-3 items-end h="66px" overflow-hidden>
+                <div
+                  :w="item.is_today ? '50px' : '38px'"
+                  :h="item.is_today ? '48px' : '36px'"
+                  mr-4
+                  :style="{
+                    backgroundPosition: item.is_today
+                      ? `-56px ${-36 + (item.day_of_week - 1) * -72}px`
+                      : `-146px ${-36 + (item.day_of_week - 1) * -72}px`,
+                    backgroundSize: item.is_today ? '247px 663px' : '',
+                    filter: item.is_today
+                      ? `drop-shadow(50px 0 var(--bew-text-1))`
+                      : `drop-shadow(38px 0 var(--bew-text-3))`,
+                  }"
+                  style="
+                    transform: translateX(-100%);
+                    background-size: 247px 663px;
+                    background-image: url(//s1.hdslb.com/bfs/static/bangumi-timeline/asserts/icons.png);
+                  "
+                />
+                <h3 :text="item.is_today ? '$bew-text-2' : '$bew-text-3'">
+                  <span text="2xl" font-bold>{{
+                    daysOfTheWeekList[item.day_of_week - 1]
+                  }}</span>
+                  <span text="base $bew-text-2" ml-2>{{ item.date }}</span>
+                </h3>
+              </div>
+              <span
+                block
+                w-full
+                h-4px
+                :bg="item.is_today ? '$bew-theme-color' : '$bew-text-3'"
+                rounded-8
+              />
+              <ul
+                grid
+                gap-4
+                :border-l="`~ 2px dashed ${
+                  item.is_today ? '$bew-theme-color-40' : '$bew-text-3'
+                }`"
+                p="t-3 l-3"
+              >
+                <li v-for="episode in item.episodes" :key="episode.season_id">
+                  <div
+                    p="x-2 y-1"
+                    w="[fit-content]"
+                    mb-2
+                    rounded-4
+                    color="$bew-theme-color"
+                    bg="$bew-theme-color-20"
+                    relative
+                    grid
+                    place-items-center
+                  >
+                    <i
+                      pos="absolute left--3"
+                      w-2
+                      h-2
+                      rounded-6
+                      :bg=" item.is_today ? '$bew-theme-color' : '$bew-text-3'"
+                      transform="~ translate-x-[calc(-0.25rem-1px)]"
+                    />
                     {{ episode.pub_time }}
                   </div>
 
