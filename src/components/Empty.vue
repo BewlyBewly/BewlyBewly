3460329c22309c6e02a2d40b584fd6df65b0b6d4
Hakadao <a578457889743@gmail.com>
2023-03-26 02:41:33 +0800

    feat: `Empty` add description and custom slot feature in component

--- src/components/Empty.vue
+++ src/components/Empty.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import browser from 'webextension-polyfill'
+const props = defineProps<{
+  description: string
+}>()
+
 const emptyImg = browser.runtime.getURL('/assets/empty.png')
 </script>
 
 <template>
-  <div flex="~" justify="center" items="center">
+  <div flex="~ col gap-4" justify="center" items="center">
     <img :src="emptyImg" w="200px" h="auto">
+    <span text="$bew-text-3">{{ props.description }}</span>
+    <slot />
   </div>
 </template>
