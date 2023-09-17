a8873b5d787e789ad100556e40245d3537e79761
Hakadao <a578457889743@gmail.com>
2023-09-18 01:37:41 +0800

    style: adjust styles * feat: support dark mode for notifications page

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -87,6 +87,8 @@ function injectApp() {
     || /https?:\/\/(www.)?bilibili.com\/account\/history.*/.test(currentUrl)
     // user space page
     || /https?:\/\/space.bilibili.com\.*/.test(currentUrl)
+    // notifications page
+    || /https?:\/\/message.bilibili.com\.*/.test(currentUrl)
   ) {
     if (
       /https?:\/\/bilibili.com\/?$/.test(currentUrl)
