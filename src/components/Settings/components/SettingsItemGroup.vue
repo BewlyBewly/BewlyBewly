a7602e6922996ad9162ee772f2d37e56fb213186
Hakadao <a578457889743@gmail.com>
2023-10-02 03:33:00 +0800

    feat: add SettingsItemGroup component

---
+++ src/components/Settings/components/SettingsItemGroup.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  desc?: string
+}>()
+</script>
+
+<template>
+  <div class="b-settings-item-group" mb-6>
+    <p text="base $bew-text-1" font-semibold>
+      {{ title }}
+    </p>
+    <p v-if="desc" text="sm $bew-text-2">
+      {{ desc }}
+    </p>
+
+    <main border="1 solid $bew-border-color" mt-2 p-4 rounded="$bew-radius" bg="$bew-elevated-solid-2" shadow="$bew-shadow-1">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+// .b-settings-item-group + .b-settings-item-group {
+//   --at-apply: mt-4
+// }
+</style>
