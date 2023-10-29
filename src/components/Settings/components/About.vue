a077a283bb88963dbdf4070b944ff13bdb47b5f5
Hakadao <a578457889743@gmail.com>
2023-10-29 19:42:43 +0800

    feat: import/export settings (#60)

--- src/components/Settings/components/About.vue
+++ src/components/Settings/components/About.vue
@@ -1,6 +1,50 @@
 <script setup lang="ts">
 import browser from 'webextension-polyfill'
 import { version } from '../../../../package.json'
+import { settings } from '~/logic'
+
+const importSettingsRef = ref<HTMLElement>()
+
+function handleImportSettings() {
+  if (importSettingsRef.value) {
+    importSettingsRef.value.click()
+
+    const handleChange = (event: Event) => {
+      const input = event.target as HTMLInputElement
+      if (input.files && input.files.length > 0) {
+        // A file has been selected
+        const selectedFile = input.files[0]
+        // Clear all previous file contents
+        input.value = ''
+
+        const reader = new FileReader()
+        reader.onload = (event: Event) => {
+          const fileReaderTarget = event.target as FileReader
+          const fileContent = fileReaderTarget.result as string
+          const jsonObject = JSON.parse(fileContent)
+
+          settings.value = jsonObject
+
+          importSettingsRef.value?.removeEventListener('change', handleChange)
+        }
+        reader.readAsText(selectedFile)
+      }
+    }
+
+    importSettingsRef.value.addEventListener('change', handleChange)
+  }
+}
+
+function handleExportSettings() {
+  const jsonStr = JSON.stringify(settings.value)
+  const blob = new Blob([jsonStr], { type: 'application/json' })
+  const url = URL.createObjectURL(blob)
+  const a = document.createElement('a')
+  a.href = url
+  a.download = 'bewly-settings.json'
+  a.click()
+  URL.revokeObjectURL(url)
+}
 </script>
 
 <template>
@@ -29,6 +73,28 @@ import { version } from '../../../../package.json'
           <tabler:brand-bilibili mr-2 /> Bilibili
         </a>
       </p>
+      <p pos="relative top-14">
+        <Button class="btn" @click="handleImportSettings">
+          <template #left>
+            <mingcute:arrow-right-down-line />
+          </template>
+          <input ref="importSettingsRef" type="file" accept=".json" hidden>
+          {{ $t('settings.import_settings') }}
+        </Button>
+        <Button class="btn" mt-2 @click="handleExportSettings">
+          <template #left>
+            <mingcute:arrow-left-up-line />
+          </template>
+          {{ $t('settings.export_settings') }}
+        </Button>
+      </p>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.btn {
+  --b-button-color: var(--bew-fill-1);
+  --b-button-color-hover: var(--bew-fill-2);
+}
+</style>
