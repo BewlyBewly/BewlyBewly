30286f98f71bcf330ac2ad7f4b3eee1416d578ca
starknt <1431880400@qq.com>
2024-01-06 15:08:30 +0800

    fix: 取消收藏功能在 Chromium 中失效

--- src/manifest.ts
+++ src/manifest.ts
@@ -67,6 +67,19 @@ export async function getManifest() {
             ? `script-src 'self' http://localhost:${port}; object-src 'self' http://localhost:${port}`
             : 'script-src \'self\'; object-src \'self\'',
         },
+    ...(isFirefox
+      ? {}
+      : {
+          declarative_net_request: {
+            rule_resources: [
+              {
+                id: 'ruleset_1',
+                enabled: true,
+                path: 'assets/rules.json',
+              },
+            ],
+          },
+        }),
   }
 
   if (isDev)
