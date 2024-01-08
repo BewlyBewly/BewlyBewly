518549cae1bb51ad1e9fa8cdeffa89f629c2be42
Hakadao <a578457889743@gmail.com>
2024-01-08 12:10:16 +0800

    feat: temporarily hide light and dark theme transition animations

--- src/components/Dock/Dock.vue
+++ src/components/Dock/Dock.vue
@@ -84,55 +84,62 @@ onMounted(() => {
   }
 })
 
+// eslint-disable-next-line unused-imports/no-unused-vars
 function toggleDark(e: MouseEvent) {
-  const isAppearanceTransition = typeof document !== 'undefined'
-  // @ts-expect-error: Transition API
-  && document.startViewTransition
-  && !window.matchMedia('(prefers-reduced-motion: reduce)').matches
-  if (!isAppearanceTransition) {
-    if (currentAppColorScheme.value === 'light')
-      settings.value.theme = 'dark'
-    else
-      settings.value.theme = 'light'
-  }
-  else {
-    const x = e.clientX
-    const y = e.clientY
-    const endRadius = Math.hypot(
-      Math.max(x, innerWidth - x),
-      Math.max(y, innerHeight - y),
-    )
-    const isDark = currentAppColorScheme.value === 'dark'
-    // @ts-expect-error: Transition API
-    const transition = document.startViewTransition(async () => {
-      if (currentAppColorScheme.value === 'light')
-        settings.value.theme = 'dark'
-      else
-        settings.value.theme = 'light'
-      await nextTick()
-    })
+  if (currentAppColorScheme.value === 'light')
+    settings.value.theme = 'dark'
+  else
+    settings.value.theme = 'light'
 
-    transition.ready.then(() => {
-      const clipPath = [
-      `circle(0px at ${x}px ${y}px)`,
-      `circle(${endRadius}px at ${x}px ${y}px)`,
-      ]
-      document.documentElement.animate(
-        {
-          clipPath: isDark
-            ? [...clipPath].reverse()
-            : clipPath,
-        },
-        {
-          duration: 400,
-          easing: 'ease-in',
-          pseudoElement: isDark
-            ? '::view-transition-old(root)'
-            : '::view-transition-new(root)',
-        },
-      )
-    })
-  }
+  // const isAppearanceTransition = typeof document !== 'undefined'
+  //   // @ts-expect-error: Transition API
+  //   && document.startViewTransition
+  //   && !window.matchMedia('(prefers-reduced-motion: reduce)').matches
+
+  // if (!isAppearanceTransition) {
+  //   if (currentAppColorScheme.value === 'light')
+  //     settings.value.theme = 'dark'
+  //   else
+  //     settings.value.theme = 'light'
+  // }
+  // else {
+  //   const x = e.clientX
+  //   const y = e.clientY
+  //   const endRadius = Math.hypot(
+  //     Math.max(x, innerWidth - x),
+  //     Math.max(y, innerHeight - y),
+  //   )
+  //   const isDark = currentAppColorScheme.value === 'dark'
+  //   // @ts-expect-error: Transition API
+  //   const transition = document.startViewTransition(async () => {
+  //     if (currentAppColorScheme.value === 'light')
+  //       settings.value.theme = 'dark'
+  //     else
+  //       settings.value.theme = 'light'
+  //     await nextTick()
+  //   })
+
+  //   transition.ready.then(() => {
+  //     const clipPath = [
+  //     `circle(0px at ${x}px ${y}px)`,
+  //     `circle(${endRadius}px at ${x}px ${y}px)`,
+  //     ]
+  //     document.documentElement.animate(
+  //       {
+  //         clipPath: isDark
+  //           ? [...clipPath].reverse()
+  //           : clipPath,
+  //       },
+  //       {
+  //         duration: 400,
+  //         easing: 'ease-in',
+  //         pseudoElement: isDark
+  //           ? '::view-transition-old(root)'
+  //           : '::view-transition-new(root)',
+  //       },
+  //     )
+  //   })
+  // }
 }
 
 function toggleDockHide(hide: boolean) {
