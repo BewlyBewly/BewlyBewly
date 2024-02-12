26b60b79eb7c8cc1e1a577fb22fbddb293d464ac
Hakadao <a578457889743@gmail.com>
2024-02-12 16:40:41 +0800

    fix: resolve style conflicts issue

--- src/styles/adaptedStyles/index.ts
+++ src/styles/adaptedStyles/index.ts
@@ -14,32 +14,44 @@ async function setupStyles() {
     || /https?:\/\/www.bilibili.com\/?$/.test(currentUrl)
     || /https?:\/\/www.bilibili.com\/index.html$/.test(currentUrl)
     || /https?:\/\/bilibili.com\/\?spm_id_from=.*/.test(currentUrl)
-    || /https?:\/\/www.bilibili.com\/\?spm_id_from=(.)*/.test(currentUrl))
+    || /https?:\/\/www.bilibili.com\/\?spm_id_from=(.)*/.test(currentUrl)) {
     await import('./homePage.scss')
+    document.documentElement.classList.add('homePage')
+  }
 
   // notifications page
-  else if (/https?:\/\/message.bilibili.com\.*/.test(currentUrl))
+  else if (/https?:\/\/message.bilibili.com\.*/.test(currentUrl)) {
     await import('./notificationsPage.scss')
+    document.documentElement.classList.add('notificationsPage')
+  }
 
   // moments page
   else if (
     // moments
     /https?:\/\/t.bilibili.com\.*/.test(currentUrl)
     // moment detail
-    || /https?:\/\/www.bilibili.com\/opus\/.*/.test(currentUrl))
+    || /https?:\/\/www.bilibili.com\/opus\/.*/.test(currentUrl)) {
     await import('./momentsPage.scss')
+    document.documentElement.classList.add('momentsPage')
+  }
 
   // history page
-  else if (/https?:\/\/(www.)?bilibili.com\/account\/history.*/.test(currentUrl))
+  else if (/https?:\/\/(www.)?bilibili.com\/account\/history.*/.test(currentUrl)) {
     await import('./historyPage.scss')
+    document.documentElement.classList.add('historyPage')
+  }
 
   // user space page
-  else if (/https?:\/\/space.bilibili.com\.*/.test(currentUrl))
+  else if (/https?:\/\/space.bilibili.com\.*/.test(currentUrl)) {
     await import('./userSpacePage.scss')
+    document.documentElement.classList.add('userSpacePage')
+  }
 
   // search page
-  else if (/https?:\/\/search.bilibili.com\.*/.test(currentUrl))
+  else if (/https?:\/\/search.bilibili.com\.*/.test(currentUrl)) {
     await import('./searchPage.scss')
+    document.documentElement.classList.add('searchPage')
+  }
 
   // video page
   else if (
@@ -48,28 +60,38 @@ async function setupStyles() {
     || /https?:\/\/(www.)?bilibili.com\/list\/watchlater.*/.test(currentUrl)
     // favorite playlist
     || /https?:\/\/(www.)?bilibili.com\/list\/ml.*/.test(currentUrl)
-  )
+  ) {
     await import('./videoPage.scss')
+    document.documentElement.classList.add('videoPage')
+  }
 
   else if (
     // anime playback & movie page
     /https?:\/\/(www.)?bilibili.com\/bangumi\/play\/.*/.test(currentUrl)
-  )
+  ) {
     await import('./animePlayback&MoviePage.scss')
+    document.documentElement.classList.add('animePlaybackAndMoviePage')
+  }
 
   // anime page & chinese anime page
   else if (
-    /https?:\/\/(www.)?bilibili.com\/(anime|guochuang).*/.test(currentUrl))
+    /https?:\/\/(www.)?bilibili.com\/(anime|guochuang).*/.test(currentUrl)) {
     await import('./animePage.scss')
+    document.documentElement.classList.add('animePage')
+  }
 
   // channel page e.g. tv shows, movie, variety shows, mooc page
   else if (
-    /https?:\/\/(www.)?bilibili.com\/(tv|movie|variety|mooc|documentary).*/.test(currentUrl))
+    /https?:\/\/(www.)?bilibili.com\/(tv|movie|variety|mooc|documentary).*/.test(currentUrl)) {
     await import('./channelPage.scss')
+    document.documentElement.classList.add('channelPage')
+  }
 
   // articles page
-  else if (/https?:\/\/(www.)?bilibili.com\/read.*/.test(currentUrl))
+  else if (/https?:\/\/(www.)?bilibili.com\/read.*/.test(currentUrl)) {
     await import('./articlesPage.scss')
+    document.documentElement.classList.add('articlesPage')
+  }
 }
 
 setupStyles()
