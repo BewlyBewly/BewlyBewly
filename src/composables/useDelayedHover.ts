4a1f0a278ce413eeb54a7d50d49618afd7bc841e
starknt <1431880400@qq.com>
2024-04-06 21:39:58 +0800

    feat: add delay hover for topbar

---
+++ src/composables/useDelayedHover.ts
@@ -0,0 +1,37 @@
+export function useDelayedHover({ delay = 200, enter, leave }: { delay?: number, enter: Function, leave: Function }) {
+  const el = ref<HTMLElement>()
+
+  let timer: any | undefined
+  function handleMouseEnter() {
+    if (timer) {
+      clearTimeout(timer)
+      timer = undefined
+    }
+    timer = setTimeout(() => {
+      enter()
+    }, delay)
+  }
+  function handleMouseLeave() {
+    if (timer) {
+      leave()
+      clearTimeout(timer)
+      timer = undefined
+    }
+  }
+
+  watch(el, (el) => {
+    if (el) {
+      el.addEventListener('mouseenter', handleMouseEnter)
+      el.addEventListener('mouseleave', handleMouseLeave)
+    }
+
+    return () => {
+      if (el) {
+        el.removeEventListener('mouseenter', handleMouseEnter)
+        el.removeEventListener('mouseleave', handleMouseLeave)
+      }
+    }
+  }, { flush: 'post' })
+
+  return el
+}
