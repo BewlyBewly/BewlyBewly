b6b7ed7c5abbc098f081b845982048ff5ba17ae9
Hakadao <a578457889743@gmail.com>
2024-04-07 16:35:24 +0800

    style: adjust style of frosted glasses

--- src/components/VideoCard/VideoCard.vue
+++ src/components/VideoCard/VideoCard.vue
@@ -174,12 +174,12 @@ function handleUndo() {
         <img
           :src="`${removeHttpFromUrl(cover)}@672w_378h_1c`" alt=""
           w-full h-full object-cover pos="absolute top-0 left-0" aspect-video
-          z--1
+          z--1 rounded-inherit
         >
 
         <div
           pos="absolute top-0 left-0" w-full h-full flex="~ col gap-2 items-center justify-center"
-          bg="$bew-fill-4" backdrop-blur-20px mix-blend-overlay
+          bg="$bew-fill-4" backdrop-blur-20px mix-blend-luminosity
         >
           <p mb-2 color-white text-lg>
             {{ $t('home.video_removed') }}
