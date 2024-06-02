0d312fe26bce5a48a72d26258ff34db4eba63270
hakadao <a578457889743@gmail.com>
2024-06-03 01:33:55 +0800

    feat(TopBar): redesign logo style

--- src/components/TopBar/TopBar.vue
+++ src/components/TopBar/TopBar.vue
@@ -419,10 +419,9 @@ defineExpose({
               v-show="showLogo"
               ref="logo" href="//www.bilibili.com"
               class="group logo"
-              style="backdrop-filter: var(--bew-filter-glass-1)"
-              flex items-center border="1 $bew-border-color"
-              rounded="50px" p="x-4" shadow="$bew-shadow-2" duration-300
-              bg="$bew-elevated-1 hover:$bew-theme-color dark-hover:white"
+              flex items-center border="1 transparent hover:$bew-border-color"
+              rounded="50px" p="x-4" shadow="hover:$bew-shadow-2" duration-300
+              bg="hover:$bew-theme-color dark-hover:white"
               w-auto h-50px transform-gpu
             >
               <svg
