9b5cd227466c29e23c1146752371e4eee57eb313
夜晚的鸡鸣 <110297461+MidnightCrowing@users.noreply.github.com>
2024-06-16 16:26:59 +0800

    feat: Add styles support for articles list page and articles ranking page

--- src/styles/adaptedStyles/index.ts
+++ src/styles/adaptedStyles/index.ts
@@ -98,6 +98,18 @@ async function setupStyles() {
     document.documentElement.classList.add('channelPage')
   }
 
+  // articles list page 专栏列表页
+  else if (/https?:\/\/(?:www\.)?bilibili\.com\/read\/readlist.*/.test(currentUrl)) {
+    await import('./articlesListPage.scss')
+    document.documentElement.classList.add('articlesListPage')
+  }
+
+  // articles ranking page 专栏排行榜页
+  else if (/https?:\/\/(?:www\.)?bilibili\.com\/read\/ranking.*/.test(currentUrl)) {
+    await import('./articlesRankingPage.scss')
+    document.documentElement.classList.add('articlesRankingPage')
+  }
+
   // articles page 专栏页
   else if (/https?:\/\/(?:www\.)?bilibili\.com\/read.*/.test(currentUrl)) {
     await import('./articlesPage.scss')
