76821b521fbf467bb127995ed8b7e7240ac2e491
MidnightCrowing <110297461+MidnightCrowing@users.noreply.github.com>
2024-07-08 15:12:32 +0800

    fix: prevent the list item bouncing problem in top bar popups (#889)
    * fix(TopBarPop): fade in animation
    
    * Revert "fix(TopBarPop): fade in animation"
    
    This reverts commit f3a6b340de0ff3e47f26977c5e161a35dbf987a9.
    
    * add a transparent block
    
    * chore: reduce redundant code
    
    ---------
    
    Co-authored-by: Hakadao <a578457889743@gmail.com>

--- src/components/TopBar/components/HistoryPop.vue
+++ src/components/TopBar/components/HistoryPop.vue
@@ -236,12 +236,17 @@ function getHistoryList(type: Business, view_at = 0 as number) {
         />
 
         <!-- historys -->
+
+        <!-- Use a transparent `div` instead of `margin-top` to prevent the list item bouncing problem -->
+        <!-- https://github.com/BewlyBewly/BewlyBewly/pull/889#issue-2394127922 -->
+        <div v-if="!isLoading && historys.length > 0" min-h="50px" />
+
         <TransitionGroup name="list">
           <a
             v-for="historyItem in historys"
             :key="historyItem.kid"
             :href="getHistoryUrl(historyItem)" :target="isHomePage() ? '_blank' : '_self'" rel="noopener noreferrer"
-            m="last:b-4 first:t-50px" p="2"
+            m="last:b-4" p="2"
             rounded="$bew-radius"
             hover:bg="$bew-fill-2"
             duration-300
