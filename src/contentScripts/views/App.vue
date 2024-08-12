0b8999d751f5608b68cb3ca86049e98fe8c3d6e5
Hakadao <a578457889743@gmail.com>
2024-08-13 00:05:21 +0800

    refactor: use `color-mix` for theme color & status color

--- src/contentScripts/views/App.vue
+++ src/contentScripts/views/App.vue
@@ -153,13 +153,9 @@ function setAppThemeColor() {
   const bewlyElement = document.querySelector('#bewly') as HTMLElement
   if (bewlyElement) {
     bewlyElement.style.setProperty('--bew-theme-color', settings.value.themeColor)
-    for (let i = 0; i < 9; i++)
-      bewlyElement.style.setProperty(`--bew-theme-color-${i + 1}0`, hexToHSL(settings.value.themeColor, i * 0.1 + 0.1))
   }
 
   document.documentElement.style.setProperty('--bew-theme-color', settings.value.themeColor)
-  for (let i = 0; i < 9; i++)
-    document.documentElement.style.setProperty(`--bew-theme-color-${i + 1}0`, hexToHSL(settings.value.themeColor, i * 0.1 + 0.1))
 }
 
 function handleBackToTop(targetScrollTop = 0 as number) {
