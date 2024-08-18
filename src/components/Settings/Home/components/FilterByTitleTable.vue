7ae8e1c1ea84067d364ded5403ff913108c577dd
Hakadao <a578457889743@gmail.com>
2024-08-19 02:25:43 +0800

    feat: update ui

--- src/components/Settings/Home/components/FilterByTitleTable.vue
+++ src/components/Settings/Home/components/FilterByTitleTable.vue
@@ -76,26 +76,8 @@ onKeyStroke('Escape', (e: KeyboardEvent) => {
 </script>
 
 <template>
-  <List
-    highlight-first
-    pin-top
-    w-full max-h-400px overflow-overlay
-  >
-    <ListItem min-h-44px>
-      <div max-w-50px>
-        index
-      </div>
-      <div>title</div>
-      <div>remark</div>
-      <div max-w-80px>
-        action
-      </div>
-    </ListItem>
-
-    <ListItem>
-      <div max-w-50px>
-        0
-      </div>
+  <div>
+    <div flex="~ gap-1" bg="$bew-fill-1" p-2 mb-4 rounded="$bew-radius">
       <Input
         v-model="addingFilter.keyword"
         size="small"
@@ -117,75 +99,89 @@ onKeyStroke('Escape', (e: KeyboardEvent) => {
           <template #left>
             <i i-mingcute:add-line />
           </template>
-        </Button>
-        <Button size="small" type="tertiary" @click="handleClearAddingFilter">
-          <template #left>
-            <i i-mingcute:broom-line />
-          </template>
+          Add
         </Button>
       </div>
-    </ListItem>
-    <ListItem
-      v-for="(item, index) in settings.filterByTitle" :key="item.keyword"
-      :style="{
-        background: editingIndex === index ? 'var(--bew-theme-color-20) !important' : '',
-      }"
+    </div>
+
+    <List
+      highlight-first
+      pin-top
+      w-full max-h-400px overflow-overlay
     >
-      <div max-w-50px>
-        {{ index + 1 }}
-      </div>
-      <template v-if="editingIndex === index">
-        <Input
-          ref="keywordRef"
-          v-model="editingFilter.keyword"
-          size="small"
-          placeholder="title"
-          w-full
-          @enter="handleConfirmFilter(index)"
-        />
-        <Input
-          ref="remarkRef"
-          v-model="editingFilter.remark"
-          size="small"
-          placeholder="remark"
-          w-full
-          @enter="handleConfirmFilter(index)"
-        />
-      </template>
-      <template v-else>
-        <div break-anywhere @dblclick="handleEditFilter(index, 'keyword')">
-          {{ item.keyword }}
+      <ListItem min-h-44px>
+        <div max-w-50px>
+          index
         </div>
-        <div break-anywhere @dblclick="handleEditFilter(index, 'remark')">
-          {{ item.remark }}
+        <div>title</div>
+        <div>remark</div>
+        <div max-w-80px>
+          action
         </div>
-      </template>
-      <div flex="~ gap-1" max-w-80px>
+      </ListItem>
+
+      <ListItem
+        v-for="(item, index) in settings.filterByTitle" :key="item.keyword"
+        :style="{
+          background: editingIndex === index ? 'var(--bew-theme-color-20) !important' : '',
+        }"
+      >
+        <div max-w-50px>
+          {{ index }}
+        </div>
         <template v-if="editingIndex === index">
-          <Button size="small" type="tertiary" @click="handleConfirmFilter(index)">
-            <template #left>
-              <i i-mingcute:check-line />
-            </template>
-          </Button>
-          <Button size="small" type="tertiary" @click="editingIndex = -2">
-            <template #left>
-              <i i-mingcute:close-line />
-            </template>
-          </Button>
+          <Input
+            ref="keywordRef"
+            v-model="editingFilter.keyword"
+            size="small"
+            placeholder="title"
+            w-full
+            @enter="handleConfirmFilter(index)"
+          />
+          <Input
+            ref="remarkRef"
+            v-model="editingFilter.remark"
+            size="small"
+            placeholder="remark"
+            w-full
+            @enter="handleConfirmFilter(index)"
+          />
         </template>
         <template v-else>
-          <Button size="small" type="tertiary" @click="handleEditFilter(index)">
-            <template #left>
-              <i i-mingcute:edit-2-line />
-            </template>
-          </Button>
-          <Button size="small" type="tertiary" @click="handleDeleteFilter(index)">
-            <template #left>
-              <i i-mingcute:delete-2-line />
-            </template>
-          </Button>
+          <div break-anywhere @dblclick="handleEditFilter(index, 'keyword')">
+            {{ item.keyword }}
+          </div>
+          <div break-anywhere @dblclick="handleEditFilter(index, 'remark')">
+            {{ item.remark }}
+          </div>
         </template>
-      </div>
-    </ListItem>
-  </List>
+        <div flex="~ gap-1" max-w-80px>
+          <template v-if="editingIndex === index">
+            <Button size="small" type="tertiary" @click="handleConfirmFilter(index)">
+              <template #left>
+                <i i-mingcute:check-line />
+              </template>
+            </Button>
+            <Button size="small" type="tertiary" @click="editingIndex = -2">
+              <template #left>
+                <i i-mingcute:close-line />
+              </template>
+            </Button>
+          </template>
+          <template v-else>
+            <Button size="small" type="tertiary" @click="handleEditFilter(index)">
+              <template #left>
+                <i i-mingcute:edit-2-line />
+              </template>
+            </Button>
+            <Button size="small" type="tertiary" @click="handleDeleteFilter(index)">
+              <template #left>
+                <i i-mingcute:delete-2-line />
+              </template>
+            </Button>
+          </template>
+        </div>
+      </ListItem>
+    </List>
+  </div>
 </template>
