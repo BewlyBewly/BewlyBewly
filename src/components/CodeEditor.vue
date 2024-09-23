90b6cf3ed8f366e2f85e21e5b05055db9e998539
Hakadao <a578457889743@gmail.com>
2024-09-23 15:37:53 +0800

    fix: resolve #1022 by removing codemirror close #1022 erm, what the sigma?

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -1,48 +1,52 @@
 <script setup lang="ts">
-import { css } from '@codemirror/lang-css'
-import { javascript } from '@codemirror/lang-javascript'
-import { oneDark } from '@codemirror/theme-one-dark'
-import { computed, ref, watch } from 'vue'
-import { Codemirror } from 'vue-codemirror'
+// import { computed, ref, watch } from 'vue'
+// import { Codemirror } from 'vue-codemirror'
 
-const props = defineProps<{
-  modelValue: string
-  language?: string
+defineProps<{
+  modelValue: string | number
+  // language?: string
 }>()
 
-const emit = defineEmits(['update:modelValue'])
+// const emit = defineEmits(['update:modelValue'])
 
-const code = ref(props.modelValue)
+const model = defineModel<string | number>()
 
-watch(() => props.modelValue, (newValue) => {
-  if (newValue !== code.value)
-    code.value = newValue
-})
+// const code = ref(props.modelValue)
 
-const extensions = computed(() => {
-  const exts = []
-  if (props.language === 'css')
-    exts.push(css())
-  else
-    exts.push(javascript())
+// watch(() => props.modelValue, (newValue) => {
+//   if (newValue !== code.value)
+//     code.value = newValue
+// })
 
-  exts.push(oneDark)
-  return exts
-})
+// const extensions = computed(() => {
+//   const exts = []
+//   if (props.language === 'css')
+//     exts.push(css())
+//   else
+//     exts.push(javascript())
 
-function onChange(value: string) {
-  code.value = value
-  emit('update:modelValue', value)
-}
+//   exts.push(oneDark)
+//   return exts
+// })
+
+// function onChange(value: string) {
+//   code.value = value
+//   emit('update:modelValue', value)
+// }
 </script>
 
 <template>
-  <Codemirror
+  <!-- <Codemirror
     v-model="code"
     :extensions="extensions"
     :indent-with-tab="true"
     :tab-size="2"
     :style="{ height: '500px', border: '1px solid var(--bew-border-color)', borderRadius: '4px !important' }"
     @change="onChange"
+  /> -->
+  <textarea
+    v-model="model"
+    w-full h-500px border="1 solid $bew-border-color" rounded="4px" p-2
+    outline-none
   />
 </template>
