40d2c165a33832437a63b3650a98109a7545c1a6
Hakadao <a578457889743@gmail.com>
2024-12-04 23:05:25 +0800

    fix(top-bar): some stupid and fucking weird bugs

--- src/components/TopBar/TopBar.vue
+++ src/components/TopBar/TopBar.vue
@@ -66,8 +66,12 @@ const forceWhiteIcon = computed((): boolean => {
   if (
     // 分區頁面由於上面有背景，所以強制 icon 變白色用於區分背景
     // channel, anime, chinese anime, tv shows, movie, variety shows, mooc but not including video page
-    /https?:\/\/(?:www.)?bilibili.com\/(?:v|anime|guochuang|tv|movie|variety|mooc).*/.test(location.href)
-    && !/https?:\/\/(?:www.)?bilibili.com\/video.*/.test(location.href)
+    (
+      /https?:\/\/(?:www.)?bilibili.com\/(?:v|anime|guochuang|tv|movie|variety|mooc).*/.test(location.href)
+      && !/https?:\/\/(?:www.)?bilibili.com\/video.*/.test(location.href)
+    )
+    // watch later & history page
+    || /https?:\/\/(?:www.)?bilibili.com\/(?:watchlater|account\/history).*/.test(location.href)
   ) {
     return true
   }
