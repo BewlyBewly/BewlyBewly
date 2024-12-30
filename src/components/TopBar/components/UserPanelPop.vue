6344eea01bbe14661cb4e3248a693be12b8e793d
Hakadao <a578457889743@gmail.com>
2024-12-30 11:34:52 +0800

    fix(user-panel-pop): panel too tall on firefox

--- src/components/TopBar/components/UserPanelPop.vue
+++ src/components/TopBar/components/UserPanelPop.vue
@@ -129,9 +129,9 @@ function handleClickChannel() {
 
 <template>
   <div
-    style="backdrop-filter: var(--bew-filter-glass-1);"
-    h="[calc(100vh-100px)]" max-h-max important-overflow-y-auto
-    p-4 rounded="$bew-radius" w-300px z--1 bg="$bew-elevated-alt"
+    style="backdrop-filter: var(--bew-filter-glass-1); max-height: 560px;"
+    w-300px h="[calc(100vh-100px)]" important-overflow-y-auto
+    p-4 rounded="$bew-radius" z--1 bg="$bew-elevated-alt"
     border="1 $bew-border-color"
     shadow="[var(--bew-shadow-3),var(--bew-shadow-edge-glow-1)]"
   >
