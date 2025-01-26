27807fbd64fc77f7f48899a2f810f2c18e478002
Hakadao <a578457889743@gmail.com>
2025-01-26 19:25:55 +0800

    chore: tweak styles

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -10,6 +10,12 @@ const model = defineModel<string | number>()
   <textarea
     v-model="model"
     w-full h-500px border="1 solid $bew-border-color" rounded="4px" p-2
-    outline-none
+    outline-none bg="$bew-fill-1"
   />
 </template>
+
+<style lang="scss" scoped>
+textarea {
+  font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace; /* 1 */
+}
+</style>
