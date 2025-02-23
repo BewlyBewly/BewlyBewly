3428698662e61b8cb8443b63b4043c09313e9e90
stupid clown <ventusuta@gmail.com>
2025-02-23 02:30:44 -0500

    refactor: font adjust (#1328)
    Co-authored-by: Hakadao <a578457889743@gmail.com>

--- src/styles/injectBuildInFonts.ts
+++ src/styles/injectBuildInFonts.ts
@@ -4,18 +4,24 @@ import { injectCSS } from '~/utils/main'
 
 injectCSS(`
   @font-face {
-    font-family: "Geist-Number";
-    unicode-range: U+0030-0039, U+FF10-FF19;
-    src: url(${browser.runtime.getURL('/assets/fonts/Geist-VF.ttf')}) format("truetype-variations");
+    font-family: "Numbers";
+    unicode-range: U+0030-0039;
+    src: url(${browser.runtime.getURL('/assets/fonts/Geist[wght].woff2')}) format("woff2-variations");
   }
 
   @font-face {
     font-family: "Onest";
-    src: url(${browser.runtime.getURL('/assets/fonts/Onest-VF.ttf')}) format("truetype-variations");
+    src: url(${browser.runtime.getURL('/assets/fonts/Onest[wght].woff2')}) format("woff2-variations");
   }
 
   @font-face {
-    font-family: "ShangguSans";
-    src: url(${browser.runtime.getURL('/assets/fonts/ShangguSansHWSC-VF.ttf')}) format("truetype-variations");
+    font-family: "ShangguSansSCVF";
+    src: url(${browser.runtime.getURL('/assets/fonts/ShangguSansSC-VF.ttf')}) format("truetype-variations");
+  }
+
+  @font-face {
+    font-family: "CJKEmDash";
+    unicode-range: U+2014, U+2E3A-2E3B;
+    src: url(${browser.runtime.getURL('/assets/fonts/ZhudouSansVF-subset.woff2')}) format("woff2-variations");
   }
 `)
