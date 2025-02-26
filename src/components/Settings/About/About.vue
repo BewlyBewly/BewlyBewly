8ef5e0c0dbe3ff72849d4bf641998db163115296
Hakadao <a578457889743@gmail.com>
2025-02-26 12:13:56 +0800

    chore(about): Add "Farewell" text to version display

--- src/components/Settings/About/About.vue
+++ src/components/Settings/About/About.vue
@@ -162,7 +162,7 @@ async function checkGitHubRelease() {
             href="https://github.com/hakadao/BewlyBewly/releases" target="_blank"
             un-text="sm color-$bew-text-2 hover:color-$bew-text-3"
           >
-            v{{ version }}
+            v{{ version }} - Farewell
           </a>
         </p>
       </section>
